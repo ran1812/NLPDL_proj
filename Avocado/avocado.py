d380e08251ae0e45948c49236cceec8d5cad49e7
ran1812 <97892562+ran1812@users.noreply.github.com>
2022-12-31 23:27:47 +0800

    Add files via upload

---
+++ Avocado/avocado.py
@@ -0,0 +1,50 @@
+from corpus_utils.bpe_mapper import Tokenizer, CustomTokenizer
+from tokenizers import SentencePieceBPETokenizer, BertWordPieceTokenizer, ByteLevelBPETokenizer
+from util.args import CorpusArgument
+import logging
+from transformers import AutoTokenizer, AutoConfig, RobertaTokenizer
+from corpus_utils.merge import domain2pretrain, merge_domain_vocab, corpuswise_compare
+from corpus_utils.tokenizer_learner import Learner,BPELearner
+import re
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+
+    args = CorpusArgument()
+
+    if "roberta" or "../posttrain_clean" in args.encoder_class:
+        encoder_class = ByteLevelBPETokenizer
+    else:
+        encoder_class = BertWordPieceTokenizer
+    domain_tokenizer = CustomTokenizer(args=args, dir_path=args.root, encoder_class=encoder_class,
+                                       dataset_name=args.dataset, vocab_size=args.vocab_size)
+
+    pretrained_tokenizer = AutoTokenizer.from_pretrained(args.encoder_class)
+    pretrained_tokenizer.save_pretrained(args.vocab_path)
+
+    pretrained_config = AutoConfig.from_pretrained(args.encoder_class)
+
+    if args.use_fragment:
+
+        txt_file = os.path.join(args.root, args.dataset, "train.txt")
+        with open(txt_file, "r", encoding = 'utf-8') as f:
+            out = f.read()
+        new_string = re.sub('[^a-zA-Z0-9\n\.]', ' ', out)
+        new_string = re.sub(' +', ' ', new_string)
+        unique_words = list(new_string.strip().replace("\n", " ").split(" "))
+
+        if "roberta" or "../posttrain_clean" in args.encoder_class:
+            learner = BPELearner(args, pretrained_config, pretrained_tokenizer, domain_tokenizer.encoder)
+        else:
+            learner = Learner(args, pretrained_config, pretrained_tokenizer, domain_tokenizer.encoder, )
+        added_vocab = learner.update_tokenizer(unique_words, 50)
+        print(len(added_vocab))
+        d2p = domain2pretrain(added_vocab, pretrained_tokenizer, vocab_path=args.vocab_path)
+        # merge_domain_vocab(args, pretrained_tokenizer, pretrained_config, d2p, args.vocab_path)
+
+    else:
+        new_vocab = corpuswise_compare(pretrained_tokenizer, domain_tokenizer.encoder)
+        d2p = domain2pretrain(new_vocab, pretrained_tokenizer, vocab_path=args.vocab_path)
+        # merge_domain_vocab(args, pretrained_tokenizer, pretrained_config, d2p, args.vocab_path)
